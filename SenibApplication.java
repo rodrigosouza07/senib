2bde8f1f9447245f20a0dba95fd9a0176a83db47
rodrigo souza <rodrigo_souza07@outlook.com>
2023-01-26 22:29:52 -0400

    Inciando o projeto

---
+++ SenibApplication.java
@@ -0,0 +1,11 @@
+package com.nibsantoantonio.senib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class SenibApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SenibApplication.class, args);
+	}
+
+}
