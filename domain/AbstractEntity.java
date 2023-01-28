bfb2ff8dfb3e1cf84d63b7b1a9f460e2aa134525
rodrigo souza <rodrigo_souza07@outlook.com>
2023-01-27 23:20:01 -0400

    Adicionando classe aluno

--- domain/AbstractEntity.java
+++ domain/AbstractEntity.java
@@ -1,4 +1,42 @@
 package com.nibsantoantonio.senib.domain;
 
-public class AbstractEntity{
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@MappedSuperclass
+public abstract class AbstractEntity <ID extends Serializable> implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private ID id;
+
+    public ID getId() {
+        return id;
+    }
+
+    public void setId(ID id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AbstractEntity<?> that)) return false;
+        return Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "id = " + id;
+    }
 }
---
+++ domain/Aluno.java
@@ -0,0 +1,69 @@
+package com.nibsantoantonio.senib.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "aluno ")
+public class Aluno extends AbstractEntity<Long>{
+
+    @Column(name = "matricula", nullable = false, unique = true, length = 6)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long matricula;
+    @Column(name = "nome", nullable = false, length = 80)
+    public String nome;
+    @Column(name = "telefone", nullable = false, unique = true, length = 20)
+    public String telefone;
+    @Column(name = "email", nullable = false, length = 80)
+    public String email;
+    @Column(name = "GA", nullable = false, length = 40)
+    public String GA;
+    @Column(name = "senha", nullable = false, length = 16)
+    public String senha;
+    public String getNome() {
+        return nome;
+    }
+
+    public Long getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(Long matricula) {
+        this.matricula = matricula;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGA() {
+        return GA;
+    }
+
+    public void setGA(String GA) {
+        this.GA = GA;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
